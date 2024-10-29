1397acbbe69aa849ad77a9c1c35c7dec9e920e66
Artem Frenk <afrenk@wpi.edu>
2024-10-29 13:41:51 -0400

    working on 2.

--- lucas_number.java
+++ lucas_number.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 //current ideas: validation
 //trivia fact, Lucas is known also for the Fibonacci Sequence
 public class lucas_number{
+    
     public static int lucas_num (int n){
         if (n == 0){
             return 2; //L(0) = 2
@@ -14,7 +15,7 @@ public class lucas_number{
         }
     }
 
-
+    public static void calc_lucas_number(int n){}
 
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
@@ -22,11 +23,31 @@ public class lucas_number{
         System.out.println("Please enter n for the lucas numbers: ");
         int n = scanner.nextInt();
 
-        System.out.println("Calculating Lucas Sequence up to L of " + n);
+        System.out.println("Calculating Lucas Sequence up to L of " + n + "\n");
+
+        long last_time = 0;
+        int last_val = 0;
 
         for (int i = 0; i <= n; i++){
-            System.out.println(lucas_num(i) + " ");
+            long start = System.nanoTime();
+            int lucas_val = lucas_num(i);
+            long end = System.nanoTime();
+            long time_took = end -start;
+
+            System.out.println("L(" + i + ") = " + lucas_val + ", Time Taken: " + time_took + " nanoseconds");
+
+            if (i > 0){
+                double value_ratio = (double) lucas_val / last_val;
+                double time_ratio = (double) time_took / last_time;
+
+                System.out.println("L(" + i + ")/L(" + (i - 1) + ") = " + value_ratio);
+                System.out.println("Time(L(" + i + ")) / Time(L(" + (i - 1) + ")) = " + time_ratio);
+            }
+
+            last_time = time_took;
+            last_val = lucas_val;
         }
+        
         scanner.close();
     }
 }
